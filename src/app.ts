f5fae6579d9250c8b1296688d22702baff850aff
Bertram Gilfoy <nonils@tutanota.com>
2020-04-19 05:14:36 -0300

    Configurated the controller and swagger with dependency injection

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
 //src/app.ts
-import { HOST, PORT, SWAGGER_PREFIX} from './constants/system.constants';
-import express, { Application } from 'express';
+import {HOST, PORT, SWAGGER_PREFIX} from './constants/system.constants';
+import express, {Application} from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express'
-import * as swaggerDocument from './swagger.json'
 import 'reflect-metadata';
 import {Container} from "inversify";
-import {InversifyExpressServer} from "inversify-express-utils";
+import {interfaces, InversifyExpressServer, TYPE} from "inversify-express-utils";
 import {ITournamentService} from "./services/ITournamentService";
 import {TournamentServiceImpl} from "./services/impl/TournamentServiceImpl";
 import morgan from "morgan";
 
+import * as swagger from "swagger-express-ts";
+import { SwaggerDefinitionConstant } from "swagger-express-ts";
+
 // declare metadata by @controller annotation
-import "./controller/tournament.controller";
-import {log} from "util";
 import {TYPES} from "./types/types";
+import {TournamentController} from "./controller/tournament.controller";
 
 // set up container
 let container = new Container();
 
 // set up bindings
 container.bind<ITournamentService>(TYPES.ITournamentService).to(TournamentServiceImpl)
-
+container.bind<interfaces.Controller> ( TYPE.Controller )
+    .to( TournamentController ).inSingletonScope().whenTargetNamed( TournamentController.TARGET_NAME );
 
 class App {
     public app: InversifyExpressServer;
@@ -30,12 +31,11 @@ class App {
     constructor() {
         this.app = new InversifyExpressServer(container);
         this.app.setConfig(this.setConfig);
-        this.app.setConfig(this.setSwaggerConfig);
     }
 
     private setConfig(app: express.Application) {
-        app.use(bodyParser.json({ limit: '50mb' }));
-        app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+        app.use(bodyParser.json({limit: '50mb'}));
+        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
         const corsOptions: cors.CorsOptions = {
             allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
@@ -45,7 +45,7 @@ class App {
             preflightContinue: false
         };
 
-        app.use(function(req, res, next) {
+        app.use(function (req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
@@ -53,15 +53,32 @@ class App {
         app.use(cors(corsOptions));
         let logger = morgan('combined')
         app.use(logger);
+        app.use( '/api-docs/swagger', express.static( 'swagger' ) );
+        app.use( '/api-docs/swagger/assets', express.static( 'node_modules/swagger-ui-dist' ) );
+        app.use( swagger.express(
+            {
+                definition : {
+                    info : {
+                        title : "My api" ,
+                        version : "1.0"
+                    } ,
+                    externalDocs : {
+                        url : "My url"
+                    }
+                    // Models can be defined here
+                }
+            }
+        ) );
     }
 
-    private setSwaggerConfig(app: express.Application) {
-        let options = { customCssUrl: '/swagger/custom.css', customJs: '/swagger/custom.js' };
-        let swd: any = swaggerDocument;
-        swd.host = `${HOST}:${PORT}`
-        swd.basePath = `${SWAGGER_PREFIX}`;
-        app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-    }
+    /*
+        private setSwaggerConfig(app: express.Application) {
+            let options = { customCssUrl: '/swagger/custom.css', customJs: '/swagger/custom.js' };
+            let swd: any = swaggerDocument;
+            swd.host = `${HOST}:${PORT}`
+            swd.basePath = `${SWAGGER_PREFIX}`;
+            app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+        }*/
 }
 
 export default new App().app.build();
--- src/controller/tournament.controller.ts
+++ src/controller/tournament.controller.ts
@@ -4,17 +4,36 @@ import {ITournamentService} from "../services/ITournamentService";
 import {TYPES} from "../types/types";
 import {controller, httpPost, interfaces} from "inversify-express-utils";
 import {CreateNewTournamentRequest} from "./dto/CreateNewTournamentRequest";
+import {ApiOperationPost, ApiPath} from "swagger-express-ts";
 
 
+@ApiPath({
+    path: "/versions",
+    name: "Version",
+    security: { basicAuth: [] }
+})
 @controller("/tournaments")
 export class TournamentController implements interfaces.Controller{
 
+    public static TARGET_NAME: string = "TuVieja";
     private _tournamentService: ITournamentService
 
-    private constructor(@inject(TYPES.ITournamentService) tournamentService : ITournamentService) {
+    public constructor(@inject(TYPES.ITournamentService) tournamentService : ITournamentService) {
         this._tournamentService = tournamentService;
     }
 
+    @ApiOperationPost({
+        description: "Post tournament object",
+        summary: "Create a new tournament",
+        parameters: {
+            body: { description: "Create a new tournament", required: true, model: "Tournament" }
+        },
+        responses: {
+            200: { description: "Success" },
+            400: { description: "Parameters fail" },
+            404: { description: "An element was not found trying to create the tournament" }
+        }
+    })
     @httpPost("/")
     public async createNewTournament(req: Request, res:Response) {
         this._tournamentService.createTournament(new CreateNewTournamentRequest(0,
